20a7dc364ddb350130f42a0a510d09d55ab63b1a
Eric Chong <echong@Erics-MacBook-Pro.local>
2014-09-15 14:19:43 -0700

    Change app title

--- config/env/all.js
+++ config/env/all.js
@@ -2,9 +2,9 @@
 
 module.exports = {
 	app: {
-		title: 'MEAN.JS',
-		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-		keywords: 'mongodb, express, angularjs, node.js, mongoose, passport'
+		title: 'Praise & Worship',
+		description: '',
+		keywords: 'praise, worship, hymn'
 	},
 	port: process.env.PORT || 3000,
 	templateEngine: 'swig',
