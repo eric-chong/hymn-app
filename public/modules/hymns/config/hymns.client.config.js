ca16a7636a0b0d951aaed24a539e6f612fe2f837
Eric Chong <echong@Erics-MacBook-Pro.local>
2014-09-20 14:38:39 -0700

    Add publishers and hymnbooks menu item

--- public/modules/hymns/config/hymns.client.config.js
+++ public/modules/hymns/config/hymns.client.config.js
@@ -4,9 +4,7 @@
 angular.module('hymns').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		//Menus.addMenuItem('topbar', 'Articles', 'articles', 'link', '/articles', false, 'user');
-		//Menus.addMenuItem('topbar', 'Articles', 'articles', 'dropdown', '/articles(/create)?');
-		//Menus.addSubMenuItem('topbar', 'articles', 'List Articles', 'articles');
-		//Menus.addSubMenuItem('topbar', 'articles', 'New Article', 'articles/create');
+		Menus.addMenuItem('topbar', 'Publishers', 'publishers', 'link', '/publishers', true);
+		Menus.addMenuItem('topbar', 'Hymn Books', 'hymnbooks', 'link', '/hymnbooks', true);
 	}
 ]);
